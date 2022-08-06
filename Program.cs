d5e2566ea92ebe7391649ff242cc99ef52c8fa4d
Chaitra P <chaitra.p@bp.com>
2022-08-06 10:28:57 +0530

    Added flipthe coin

--- Program.cs
+++ Program.cs
@@ -5,14 +5,16 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Please select the option which program you want to exceute");
-           
+            Console.WriteLine("Option 1 : Flip the Coin");
             int option = Convert.ToInt32(Console.ReadLine());
 
            
 
             switch(option)
             {
-               
+                case 1:
+                    FlipCoin.CalculatePercentage();
+                    break;
                 default:
                     Console.WriteLine("Please enter the correct option");
                     break;
