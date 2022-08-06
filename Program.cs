94624418892f8fc51e747bc5a609d031c65a2e43
Chaitra P <chaitra.p@bp.com>
2022-08-06 10:52:34 +0530

    Added QuotientReamider

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Please select the option which program you want to exceute");
             Console.WriteLine("Option 1 : Flip the Coin");
+            Console.WriteLine("Option 2 : QuotientRemaider");
             int option = Convert.ToInt32(Console.ReadLine());
 
            
@@ -14,6 +15,9 @@
             {
                 case 1:
                     FlipCoin.CalculatePercentage();
+                    break;
+                case 2:
+                    QuotientRemainder.CalculateQuotientAndRemainder();
                     break;
                 default:
                     Console.WriteLine("Please enter the correct option");
