fde729bbfaa72828f5459d034403270c2a34998e
Chaitra P <chaitra.p@bp.com>
2022-08-06 10:24:32 +0530

    Greeting Meassage

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace BasicCoreProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please select the option which program you want to exceute");
+            Console.WriteLine("Option 1 : Flip the Coin");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+           
+
+            switch(option)
+            {
+                case 1:
+                    FlipCoin.CalculatePercentage();
+                    break;
+                default:
+                    Console.WriteLine("Please enter the correct option");
+                    break;
+            }
+        }
+    }
+}
